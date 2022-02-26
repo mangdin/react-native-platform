9bad06359dc6fd0b5311c149b017d5e3d158a7ba
mangdin <41657311+mangdin@users.noreply.github.com>
2022-02-26 16:49:28 +0800

    Update ManufacturerBackgroundManager.ts

--- src/ManufacturerBackgroundManager.ts
+++ src/ManufacturerBackgroundManager.ts
@@ -1,26 +1,27 @@
-import { NativeModules } from 'react-native'
-const { RNPlatform } = NativeModules
-import { isHuawei, isXiaomi, isVIVO, isOPPO, isMeizu } from './DeviceUtil'
-
+import { NativeModules } from 'react-native';
+const { RNPlatform } = NativeModules;
+import { isHuawei, isXiaomi, isVIVO, isOPPO, isMeizu } from './DeviceUtil';
 export function isBackgroundSettingSupported() {
-  return isHuawei() || isXiaomi() || isVIVO() || isOPPO() || isMeizu()
+    return isHuawei() || isXiaomi() || isVIVO() || isOPPO() || isMeizu();
 }
-
-export function openBackgroundSettings(): Promise<boolean> {
-  return RNPlatform.openBackgroundSettings()
+export function openBackgroundSettings() {
+    return RNPlatform.openBackgroundSettings();
 }
-
 export function backgroudSettingTip() {
-  if (isHuawei()) {
-    return '应用启动管理 -> 关闭应用开关 -> 打开允许自启动'
-  } else if (isXiaomi()) {
-    return '授权管理 -> 自启动管理 -> 允许应用自启动'
-  } else if (isVIVO()) {
-    return '权限管理 -> 自启动 -> 允许应用自启动'
-  } else if (isOPPO()) {
-    return '权限隐私 -> 自启动管理 -> 允许应用自启动'
-  } else if (isMeizu()) {
-    return '权限管理 -> 后台管理 -> 点击应用 -> 允许后台运行'
-  }
-  return ''
+    if (isHuawei()) {
+        return '应用启动管理 -> 打开【益华家长】允许开关';
+    }
+    else if (isXiaomi()) {
+        return '自启动管理 -> 打开【益华家长】允许开关';
+    }
+    else if (isVIVO()) {
+        return '应用管理 -> 权限管理 -> 自启动和悬浮窗 -> 打开【益华家长】允许开关';
+    }
+    else if (isOPPO()) {
+        return '权限隐私 -> 自启动管理 -> 允许应用自启动';
+    }
+    else if (isMeizu()) {
+        return '权限管理 -> 后台管理 -> 点击应用 -> 允许后台运行';
+    }
+    return '';
 }
