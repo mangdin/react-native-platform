b68c1b80644b4e8c0ffa370fa23f84e2f06196b7
mangdin <41657311+mangdin@users.noreply.github.com>
2022-02-26 16:58:54 +0800

    Update ManufacturerBackgroundManager.ts

--- src/ManufacturerBackgroundManager.ts
+++ src/ManufacturerBackgroundManager.ts
@@ -21,7 +21,7 @@ export function backgroudSettingTip() {
         return '权限隐私 -> 自启动管理 -> 允许应用自启动';
     }
     else if (isMeizu()) {
-        return '权限管理 -> 后台管理 -> 点击应用 -> 允许后台运行';
+        return '权限管理 -> 后台管理 -> 点击【益华家长】 -> 允许后台运行';
     }
     return '';
 }
